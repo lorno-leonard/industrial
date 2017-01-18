7c8adf2ea1b0b76a80e31637cc36d0d860587c3f
Leonard Laput <online.lorno@gmail.com>
2017-01-19 00:05:46 +0800

    style(header): update favicon

--- keystone.js
+++ keystone.js
@@ -43,7 +43,7 @@ keystone.set('routes', require('./routes'));
 
 // Configure the navigation bar in Keystone's Admin UI
 keystone.set('nav', {
-	posts: ['posts', 'post-categories'],
+	blog: ['posts', 'post-categories'],
 	galleries: 'galleries',
 	enquiries: 'enquiries',
 	users: 'users',
