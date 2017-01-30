3d0e903924668c52afddff5afd7dafe4d4faa1e6
Leonard Laput <online.lorno@gmail.com>
2017-01-30 16:46:53 +0800

    feat(home): carousel banner image

---
+++ public/js/industrial/scripts/index.js
@@ -0,0 +1,12 @@
+/* globals  $ */
+
+$(function () {
+  $('.slick').slick({
+    infinite: true,
+    centerMode: true,
+    variableWidth: true,
+    autoplay: true,
+    slidesToShow: 3,
+    slidesToScroll: 3,
+  });
+});
